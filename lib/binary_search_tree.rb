83f0d5be940692c611ffa14f97f2cab0af66fe5f
Adam DePaolo <amdepaolo@gmail.com>
2023-04-05 17:57:50 -0400

    commit tree 07682619d28188aca9d746257dcaccb8997d00fa

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -8,11 +8,49 @@ class BinarySearchTree
   end
 
   def search(value)
-    # your code here
+    node_to_check = @root
+    while node_to_check
+      if node_to_check.value == value
+        return node_to_check
+      elsif value > node_to_check.value
+        node_to_check = node_to_check.right
+      else value < node_to_check.value
+        node_to_check = node_to_check.left
+      end
+    end
+
+    return nil
   end
 
   def insert(value)
-    # your code here
+    if @root.nil?
+      @root = Node.new(value)
+      return @root
+    end
+
+    if self.search(value)
+      return nil
+    end
+
+    node_to_check = @root
+
+    while node_to_check
+      if node_to_check.value > value
+        if node_to_check.left.nil?
+          node_to_check.left = Node.new(value)
+          return node_to_check.left
+        else 
+          node_to_check = node_to_check.left
+        end
+      elsif node_to_check.value < value
+        if node_to_check.right.nil?
+          node_to_check.right = Node.new(value)
+          return node_to_check.right
+        else
+          node_to_check = node_to_check.right
+        end
+      end
+    end
   end
 
 end 
